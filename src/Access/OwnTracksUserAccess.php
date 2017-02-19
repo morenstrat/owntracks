79a897152409fb5b90a0d0e95dff2e381b6c89fc
Markus Orenstrat <markus@dunix-data.de>
2017-02-19 23:52:20 +0100

    Add custom access check.

---
+++ src/Access/OwnTracksUserAccess.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\owntracks\Access;
+
+use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * OwnTracksUserAccess definition.
+ */
+class OwnTracksUserAccess implements AccessInterface {
+
+  /**
+   * Check access to user owntracks locations map.
+   *
+   * @param AccountInterface $account
+   *   The currently logged in user.
+   *
+   * @param UserInterface $user
+   *   The owner of the owntracks location map.
+   *
+   * @return bool
+   *   TRUE if access should be granted, FALSE otherwise.
+   */
+  public function access(AccountInterface $account, UserInterface $user) {
+    if ($account->hasPermission('view any owntracks location')) {
+      return TRUE;
+    }
+
+    if ($account->hasPermission('view own owntracks locations') && $account->id() === $user->id()) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+}
