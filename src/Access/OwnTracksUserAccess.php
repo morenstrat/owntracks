dbcf6a05bbaa30bfb04b6833a06566ff25a80554
Markus Orenstrat <markus@dunix-data.de>
2017-02-20 11:01:22 +0100

    Update user access service.

--- src/Access/OwnTracksUserAccess.php
+++ src/Access/OwnTracksUserAccess.php
@@ -3,6 +3,7 @@
 namespace Drupal\owntracks\Access;
 
 use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\user\UserInterface;
 
@@ -16,19 +17,23 @@ class OwnTracksUserAccess implements AccessInterface {
    *
    * @param AccountInterface $account
    *   The currently logged in user.
-   * @param UserInterface $user
-   *   The owner of the owntracks location map.
+   * @param CurrentRouteMatch $current_route_match
+   *   The current route match service.
    *
    * @return bool
    *   TRUE if access should be granted, FALSE otherwise.
    */
-  public function access(AccountInterface $account, UserInterface $user) {
+  public function access(AccountInterface $account, CurrentRouteMatch $current_route_match) {
     if ($account->hasPermission('view any owntracks location')) {
       return TRUE;
     }
 
-    if ($account->hasPermission('view own owntracks locations') && $account->id() === $user->id()) {
-      return TRUE;
+    if ($account->hasPermission('view own owntracks locations')) {
+      $user = $current_route_match->getParameter('user');
+
+      if ($user instanceof UserInterface && $user->id() === $account->id()) {
+        return TRUE;
+      }
     }
 
     return FALSE;
