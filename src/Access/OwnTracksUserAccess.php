7e4452bef79457383b41d9465c8e8355286f62bd
Markus Orenstrat <markus@dunix-data.de>
2017-02-20 16:12:32 +0100

    Update user access service.

--- src/Access/OwnTracksUserAccess.php
+++ src/Access/OwnTracksUserAccess.php
@@ -3,7 +3,6 @@
 namespace Drupal\owntracks\Access;
 
 use Drupal\Core\Routing\Access\AccessInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\user\UserInterface;
 use Drupal\Core\Access\AccessResult;
@@ -18,26 +17,19 @@ class OwnTracksUserAccess implements AccessInterface {
    *
    * @param AccountInterface $account
    *   The currently logged in user.
-   * @param RouteMatchInterface $route_match
-   *   The route match service.
+   * @param UserInterface $user
+   *   The currently visited user.
    *
    * @return AccessResult
    *   Allowed or Forbidden.
    */
-  public function access(AccountInterface $account, RouteMatchInterface $route_match) {
-    if ($account->hasPermission('view any owntracks location')) {
+  public function access(AccountInterface $account, UserInterface $user) {
+    if ($account->hasPermission('view any owntracks location') || ($account->hasPermission('view own owntracks locations') && $user->id() === $account->id())) {
       return AccessResult::allowedIfHasPermission($account, 'access user profiles');
     }
-
-    if ($account->hasPermission('view own owntracks locations')) {
-      $user = $route_match->getParameter('user');
-
-      if ($user instanceof UserInterface && $user->id() === $account->id()) {
-        return AccessResult::allowedIfHasPermission($account, 'access user profiles');
-      }
+    else {
+      return AccessResult::forbidden();
     }
-
-    return AccessResult::forbidden();
   }
 
 }
