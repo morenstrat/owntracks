a22bcbdfb8f6fab3815da3dbe46a9a8902398727
Markus Orenstrat <markus@dunix-data.de>
2017-02-23 11:09:52 +0100

    Add field to owntracks map settings form.

--- src/OwnTracksMapSettingsForm.php
+++ src/OwnTracksMapSettingsForm.php
@@ -15,7 +15,7 @@ class OwnTracksMapSettingsForm extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return [
-      'owntracks_map.settings',
+      'owntracks.map.settings',
     ];
   }
 
@@ -30,6 +30,27 @@ class OwnTracksMapSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['tile_layer_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Tile layer url'),
+      '#default_value' => $this->config('owntracks.map.settings')->get('tileLayerUrl'),
+      '#required' => TRUE,
+    ];
+
+    $form['tile_layer_attribution'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Tile layer attribution'),
+      '#default_value' => $this->config('owntracks.map.settings')->get('tileLayerAttribution'),
+      '#required' => TRUE,
+    ];
+
+    $form['polyline_color'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Polyline color'),
+      '#default_value' => $this->config('owntracks.map.settings')->get('polylineColor'),
+      '#required' => TRUE,
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -37,6 +58,12 @@ class OwnTracksMapSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('owntracks.map.settings')
+      ->set('tileLayerUrl', $form_state->getValue('tile_layer_url'))
+      ->set('tileLayerAttribution', $form_state->getValue('tile_layer_attribution'))
+      ->set('polylineColor', $form_state->getValue('polyline_color'))
+      ->save();
+
     parent::submitForm($form, $form_state);
   }
 
