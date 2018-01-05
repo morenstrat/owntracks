edcde06f87e2e4696bb0b0c3ed79c61d758b93e7
Markus Orenstrat <markus@dunix-data.de>
2018-01-05 17:55:06 +0100

    Added entity access tests.

--- tests/src/Functional/OwnTracksAccessTest.php
+++ tests/src/Functional/OwnTracksAccessTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\owntracks\Functional;
 
+use Drupal\owntracks\Entity\OwnTracksLocation;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Core\Url;
 
@@ -27,73 +28,236 @@ class OwnTracksAccessTest extends BrowserTestBase {
    * Tests owntracks access controllers and plugins.
    */
   public function testAccess() {
-    // Prepare accounts and urls for testing user map and views.
-    $account = $this->createUser();
-    $accountMapUrl = Url::fromRoute('owntracks.user_map', ['user' => $account->id()])
-      ->setAbsolute(TRUE)->toString();
-    $accountViewsUrl = Url::fromRoute('view.owntracks_location.current', ['user' => $account->id()])
-      ->setAbsolute(TRUE)->toString();
-    $viewOwnAccount = $this->createUser(['view own owntracks entities']);
-    $viewOwnAccountMapUrl = Url::fromRoute('owntracks.user_map', ['user' => $viewOwnAccount->id()])
-      ->setAbsolute(TRUE)->toString();
-    $viewOwnAccountViewsUrl = Url::fromRoute('view.owntracks_location.current', ['user' => $viewOwnAccount->id()])
-      ->setAbsolute(TRUE)->toString();
-    $viewAnyAccount = $this->createUser(['view any owntracks entity']);
-    $viewAnyAccountMapUrl = Url::fromRoute('owntracks.user_map', ['user' => $viewAnyAccount->id()])
-      ->setAbsolute(TRUE)->toString();
-    $viewAnyAccountViewsUrl = Url::fromRoute('view.owntracks_location.current', ['user' => $viewAnyAccount->id()])
-      ->setAbsolute(TRUE)->toString();
-    $adminAccount = $this->createUser(['administer owntracks']);
-    $adminAccountMapUrl = Url::fromRoute('owntracks.user_map', ['user' => $adminAccount->id()])
-      ->setAbsolute(TRUE)->toString();
-    $adminAccountViewsUrl = Url::fromRoute('view.owntracks_location.current', ['user' => $adminAccount->id()])
+    // Create test accounts.
+    $notAccount = $this->createUser();
+
+    $ownAccount = $this->createUser([
+      'create owntracks entities',
+      'view own owntracks entities',
+      'update own owntracks entities',
+      'delete own owntracks entities',
+    ]);
+
+    $anyAccount = $this->createUser([
+      'create owntracks entities',
+      'view any owntracks entity',
+      'update any owntracks entity',
+      'delete any owntracks entity',
+    ]);
+
+    $admAccount = $this->createUser([
+      'administer owntracks',
+    ]);
+
+    // Create test entities.
+    $notEntity = OwnTracksLocation::create([
+      'uid' => $notAccount->id(),
+      'lat' => 7,
+      'lon' => 53,
+      'tst' => 123456,
+    ]);
+    $notEntity->save();
+
+    $ownEntity = OwnTracksLocation::create([
+      'uid' => $ownAccount->id(),
+      'lat' => 7,
+      'lon' => 53,
+      'tst' => 123456,
+    ]);
+    $ownEntity->save();
+
+    $anyEntity = OwnTracksLocation::create([
+      'uid' => $anyAccount->id(),
+      'lat' => 7,
+      'lon' => 53,
+      'tst' => 123456,
+    ]);
+    $anyEntity->save();
+
+    $admEntity = OwnTracksLocation::create([
+      'uid' => $admAccount->id(),
+      'lat' => 7,
+      'lon' => 53,
+      'tst' => 123456,
+    ]);
+    $admEntity->save();
+
+    // Create test urls.
+    $createEntity = Url::fromRoute('entity.owntracks_location.add_form')
       ->setAbsolute(TRUE)->toString();
 
-    // Test user without permission.
-    $this->drupalLogin($account);
-    $this->drupalGet($accountMapUrl);
+    $viewNotEntity = Url::fromRoute('entity.owntracks_location.canonical', ['owntracks_location' => $notEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $updateNotEntity = Url::fromRoute('entity.owntracks_location.edit_form', ['owntracks_location' => $notEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $deleteNotEntity = Url::fromRoute('entity.owntracks_location.delete_form', ['owntracks_location' => $notEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $viewOwnEntity = Url::fromRoute('entity.owntracks_location.canonical', ['owntracks_location' => $ownEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $updateOwnEntity = Url::fromRoute('entity.owntracks_location.edit_form', ['owntracks_location' => $ownEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $deleteOwnEntity = Url::fromRoute('entity.owntracks_location.delete_form', ['owntracks_location' => $ownEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $viewAnyEntity = Url::fromRoute('entity.owntracks_location.canonical', ['owntracks_location' => $anyEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $updateAnyEntity = Url::fromRoute('entity.owntracks_location.edit_form', ['owntracks_location' => $anyEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $deleteAnyEntity = Url::fromRoute('entity.owntracks_location.delete_form', ['owntracks_location' => $anyEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $viewAdmEntity = Url::fromRoute('entity.owntracks_location.canonical', ['owntracks_location' => $admEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $updateAdmEntity = Url::fromRoute('entity.owntracks_location.edit_form', ['owntracks_location' => $admEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+    $deleteAdmEntity = Url::fromRoute('entity.owntracks_location.delete_form', ['owntracks_location' => $admEntity->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $notAccountMap = Url::fromRoute('owntracks.user_map', ['user' => $notAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+    $notAccountView = Url::fromRoute('view.owntracks_location.current', ['user' => $notAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $ownAccountMap = Url::fromRoute('owntracks.user_map', ['user' => $ownAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+    $ownAccountView = Url::fromRoute('view.owntracks_location.current', ['user' => $ownAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $anyAccountMap = Url::fromRoute('owntracks.user_map', ['user' => $anyAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+    $anyAccountView = Url::fromRoute('view.owntracks_location.current', ['user' => $anyAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    $admAccountMap = Url::fromRoute('owntracks.user_map', ['user' => $admAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+    $admAccountView = Url::fromRoute('view.owntracks_location.current', ['user' => $admAccount->id()])
+      ->setAbsolute(TRUE)->toString();
+
+    // Test access.
+    $this->drupalLogin($notAccount);
+    $this->drupalGet($createEntity);
     $this->assertSession()->statusCodeEquals(403);
-    $this->drupalGet($viewOwnAccountMapUrl);
+    $this->drupalGet($viewNotEntity);
     $this->assertSession()->statusCodeEquals(403);
-    $this->drupalGet($accountViewsUrl);
+    $this->assertFalse($notEntity->access('view', $notAccount));
+    $this->drupalGet($viewOwnEntity);
     $this->assertSession()->statusCodeEquals(403);
-    $this->drupalGet($viewOwnAccountViewsUrl);
+    $this->assertFalse($ownEntity->access('view', $notAccount));
+    $this->drupalGet($updateNotEntity);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->assertFalse($notEntity->access('update', $notAccount));
+    $this->drupalGet($updateOwnEntity);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->assertFalse($ownEntity->access('update', $notAccount));
+    $this->drupalGet($deleteNotEntity);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->assertFalse($notEntity->access('delete', $notAccount));
+    $this->drupalGet($deleteOwnEntity);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->assertFalse($ownEntity->access('delete', $notAccount));
+
+    $this->drupalGet($notAccountMap);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet($ownAccountMap);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet($notAccountView);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet($ownAccountView);
     $this->assertSession()->statusCodeEquals(403);
 
-    // Test user with permission to view own owntracks entities.
-    $this->drupalLogin($viewOwnAccount);
-    $this->drupalGet($viewOwnAccountMapUrl);
+    $this->drupalLogin($ownAccount);
+    $this->drupalGet($createEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($viewAnyAccountMapUrl);
+    $this->drupalGet($viewOwnEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($ownEntity->access('view', $ownAccount));
+    $this->drupalGet($viewAnyEntity);
     $this->assertSession()->statusCodeEquals(403);
-    $this->drupalGet($viewOwnAccountViewsUrl);
+    $this->assertFalse($anyEntity->access('view', $ownAccount));
+    $this->drupalGet($updateOwnEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($viewAnyAccountViewsUrl);
+    $this->assertTrue($ownEntity->access('update', $ownAccount));
+    $this->drupalGet($updateAnyEntity);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->assertFalse($anyEntity->access('update', $ownAccount));
+    $this->drupalGet($deleteOwnEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($ownEntity->access('delete', $ownAccount));
+    $this->drupalGet($deleteAnyEntity);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->assertFalse($anyEntity->access('delete', $ownAccount));
+
+    $this->drupalGet($ownAccountMap);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($anyAccountMap);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet($ownAccountView);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($anyAccountView);
     $this->assertSession()->statusCodeEquals(403);
 
-    // Test user with permission to view any owntracks entity.
-    $this->drupalLogin($viewAnyAccount);
-    $this->drupalGet($viewAnyAccountMapUrl);
+    $this->drupalLogin($anyAccount);
+    $this->drupalGet($createEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($adminAccountMapUrl);
+    $this->drupalGet($viewAnyEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($viewAnyAccountViewsUrl);
+    $this->assertTrue($anyEntity->access('view', $anyAccount));
+    $this->drupalGet($viewAdmEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($adminAccountViewsUrl);
+    $this->assertTrue($admEntity->access('view', $anyAccount));
+    $this->drupalGet($updateAnyEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($anyEntity->access('update', $anyAccount));
+    $this->drupalGet($updateAdmEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($admEntity->access('update', $anyAccount));
+    $this->drupalGet($deleteAnyEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($anyEntity->access('delete', $anyAccount));
+    $this->drupalGet($deleteAdmEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($admEntity->access('delete', $anyAccount));
+
+    $this->drupalGet($anyAccountMap);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($admAccountMap);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($anyAccountView);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($admAccountView);
     $this->assertSession()->statusCodeEquals(200);
 
-    // Test user with administer owntracks permission.
-    $this->drupalLogin($adminAccount);
-    $this->drupalGet($viewAnyAccountMapUrl);
+    $this->drupalLogin($admAccount);
+    $this->drupalGet($createEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($adminAccountMapUrl);
+    $this->drupalGet($viewAnyEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($viewAnyAccountViewsUrl);
+    $this->assertTrue($anyEntity->access('view', $admAccount));
+    $this->drupalGet($viewAdmEntity);
     $this->assertSession()->statusCodeEquals(200);
-    $this->drupalGet($adminAccountViewsUrl);
+    $this->assertTrue($admEntity->access('view', $admAccount));
+    $this->drupalGet($updateAnyEntity);
     $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($anyEntity->access('update', $admAccount));
+    $this->drupalGet($updateAdmEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($admEntity->access('update', $admAccount));
+    $this->drupalGet($deleteAnyEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($anyEntity->access('delete', $admAccount));
+    $this->drupalGet($deleteAdmEntity);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertTrue($admEntity->access('delete', $admAccount));
 
-    // @todo: test entity access
+    $this->drupalGet($anyAccountMap);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($admAccountMap);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($anyAccountView);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet($admAccountView);
+    $this->assertSession()->statusCodeEquals(200);
   }
 
 }
